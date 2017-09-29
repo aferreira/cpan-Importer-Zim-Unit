a8f6de661a9aaecf6efe41bf4455b1bb95f6b560
Adriano Ferreira <a.r.ferreira@gmail.com>
2017-09-29 10:05:10 -0700

    Depend on Importer::Zim::Base 0.8.0+

--- lib/Importer/Zim/Unit.pm
+++ lib/Importer/Zim/Unit.pm
@@ -7,7 +7,7 @@ use 5.010001;
 
 BEGIN {
     require Importer::Zim::Base;
-    Importer::Zim::Base->VERSION('0.6.0');
+    Importer::Zim::Base->VERSION('0.8.0');
     our @ISA = qw(Importer::Zim::Base);
 }
 
